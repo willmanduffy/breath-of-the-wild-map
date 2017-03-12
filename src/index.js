f399fc4d5e9239f4dd904a692501312120ca1a93
Will Duffy <willmanduffy@gmail.com>
2017-03-12 03:02:18 -0400

    Install leaflet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { render } from 'react-dom';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const position = [51.505, -0.09];
+const map = (
+  <Map center={position} zoom={13}>
+    <TileLayer
+      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    />
+    <Marker position={position}>
+      <Popup>
+        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+      </Popup>
+    </Marker>
+  </Map>
 );
+
+render(map, document.getElementById('map-container'));
